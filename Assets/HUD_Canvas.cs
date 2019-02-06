10ebf82f5638cc1f6117d1c6db296e42fee6b535
Auriel117 <BlackHole117>
2019-02-06 11:26:33 +0100

    UI update 4

---
+++ Assets/HUD_Canvas.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Canvas : MonoBehaviour
+
+{
+
+    public int HUD_Scriba = 1;
+    public GameObject Current_obj;
+    public GameObject Current_obj2;
+    public GameObject Current_obj3;
+
+
+    public void CogitatorHUD()
+    {
+        HUD_Scriba++;
+        Debug.Log(HUD_Scriba);
+
+        switch (HUD_Scriba)
+        {
+            case 1:
+                Current_obj.SetActive(true);
+                Current_obj2.SetActive(false);
+                Current_obj3.SetActive(false);
+                break;
+
+            case 2:
+                Current_obj.SetActive(false);
+                Current_obj2.SetActive(true);
+                Current_obj3.SetActive(false);
+                break;
+
+            case 3:
+                Current_obj.SetActive(false);
+                Current_obj2.SetActive(false);
+                Current_obj3.SetActive(true);
+                break;
+
+
+        }
+
+    }
+
+
+
+}
