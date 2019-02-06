aca38b05aee959415b94a8db2716b093b5e70776
Auriel117 <BlackHole117>
2019-02-06 15:55:46 +0100

    UI update 7

--- Assets/Scenes Du groupe/--Aurélien--/HUD_Canvas.cs
+++ Assets/Scenes Du groupe/--Aurélien--/HUD_Canvas.cs
@@ -10,6 +10,8 @@ public class HUD_Canvas : MonoBehaviour
     public GameObject Current_obj;
     public GameObject Current_obj2;
     public GameObject Current_obj3;
+    public GameObject Current_obj4;
+    public GameObject Current_obj5;
 
 
     public void CogitatorHUD()
@@ -23,18 +25,40 @@ public class HUD_Canvas : MonoBehaviour
                 Current_obj.SetActive(true);
                 //Current_obj2.SetActive(false);
                 //Current_obj3.SetActive(false);
+                //Current_obj4.SetActive(false);
+                //Current_obj5.SetActive(false);
                 break;
 
             case 2:
                 Current_obj.SetActive(false);
                 Current_obj2.SetActive(true);
                 //Current_obj3.SetActive(false);
+                //Current_obj4.SetActive(false);
+                //Current_obj5.SetActive(false);
                 break;
 
             case 3:
                 //Current_obj.SetActive(false);
                 Current_obj2.SetActive(false);
                 Current_obj3.SetActive(true);
+                //Current_obj4.SetActive(false);
+                //Current_obj5.SetActive(false);
+                break;
+
+            case 4:
+                //Current_obj.SetActive(false);
+                //Current_obj2.SetActive(false);
+                Current_obj3.SetActive(false);
+                Current_obj4.SetActive(true);
+                //Current_obj5.SetActive(false);
+                break;
+
+            case 5:
+                //Current_obj.SetActive(false);
+                //Current_obj2.SetActive(false);
+                //Current_obj3.SetActive(false);
+                Current_obj4.SetActive(false);
+                Current_obj5.SetActive(true);
                 break;
         }
     }
