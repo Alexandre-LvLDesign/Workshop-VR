0db1f01d0312c2df73ea9f11d8683e7a5a162a7e
Queadpool <47141782+Queadpool@users.noreply.github.com>
2019-02-06 16:01:13 +0100

    Belt

---
+++ Assets/Scenes Du groupe/--Quentin--/Scripts/Belt_Manager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Belt_Manager : MonoBehaviour {
+
+    [SerializeField] VRTK_TransformFollow tf;
+    public Transform t;
+
+    private void OnEnable()
+    {
+        //tf = GetComponent<VRTK_TransformFollow>();
+        StartCoroutine(MyCouroutine());
+    }
+
+	IEnumerator MyCouroutine()
+    {
+        yield return null;
+        t = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+        
+    }
+}
