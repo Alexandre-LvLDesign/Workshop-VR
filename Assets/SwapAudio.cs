a1c4502ae1e9dab7b2691147a34df5d56e7a24af
BaberinosLeDouxTacos <39302412+BaberinosLeDouxTacos@users.noreply.github.com>
2019-02-07 16:03:30 +0100

    Script Changement Music

---
+++ Assets/SwapAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapAudio : MonoBehaviour 
+{
+    private float AVolume1 = 1f;
+    private float AVolume2 = 0;
+    public AudioClip BarClip;
+    public AudioClip ThermesClip;
+    public AudioSource m_Bar;
+    private void OnTriggerEnter(Collider other) 
+    {
+
+        if (m_Bar.clip = ThermesClip)
+        {
+            m_Bar.clip = BarClip;
+            m_Bar.Play();
+        }
+        if (m_Bar.clip = BarClip)
+        {
+            m_Bar.clip = ThermesClip;
+            m_Bar.Play();
+
+        }
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        m_Bar.Stop();
+        if (m_Bar.clip = ThermesClip) 
+        {
+            m_Bar.clip = BarClip;
+            m_Bar.Play();
+        }
+    }
+
+}
