cb3d4aa3cb370a7f230c7b7d42b0f0efa7350ef9
Queadpool <47141782+Queadpool@users.noreply.github.com>
2019-02-12 14:02:48 +0100

    Script Obj

--- Assets/Scenes Du groupe/--Aurélien--/HUD_Canvas.cs
+++ Assets/Scenes Du groupe/--Aurélien--/HUD_Canvas.cs
@@ -13,6 +13,48 @@ public class HUD_Canvas : MonoBehaviour
     public GameObject Current_obj4;
     public GameObject Current_obj5;
 
+    private bool obj1 = true;
+    private bool obj2 = false;
+    private bool obj3 = false;
+    private bool obj4 = false;
+    private bool obj5 = false;
+
+    /*public void Update()
+    {
+        if(obj1 && "Récupérer câble")
+        {
+            obj1 = false;
+            CogitatorHUD();
+            obj2 = true;
+        }
+
+        if (obj2 && "Combiner tablette + câble")
+        {
+            obj2 = false;
+            CogitatorHUD();
+            obj3 = true;
+        }
+
+        if (obj3 && "Trouver casier")
+        {
+            obj3 = false;
+            CogitatorHUD();
+            obj4 = true;
+        }
+
+        if (obj4 && "Hacker casier")
+        {
+            obj4 = false;
+            CogitatorHUD();
+            obj5 = true;
+        }
+
+        if (obj5 && "Récupérer brochure")
+        {
+            obj5 = false;
+            CogitatorHUD();
+        }
+    }*/
 
     public void CogitatorHUD()
     {
@@ -21,42 +63,27 @@ public class HUD_Canvas : MonoBehaviour
 
         switch (HUD_change)
         {
+            //case 1 pas utile !! Jamais récupéré !!
             case 1:
                 Current_obj.SetActive(true);
-                //Current_obj2.SetActive(false);
-                //Current_obj3.SetActive(false);
-                //Current_obj4.SetActive(false);
-                //Current_obj5.SetActive(false);
                 break;
 
             case 2:
                 Current_obj.SetActive(false);
                 Current_obj2.SetActive(true);
-                //Current_obj3.SetActive(false);
-                //Current_obj4.SetActive(false);
-                //Current_obj5.SetActive(false);
                 break;
 
             case 3:
-                //Current_obj.SetActive(false);
                 Current_obj2.SetActive(false);
                 Current_obj3.SetActive(true);
-                //Current_obj4.SetActive(false);
-                //Current_obj5.SetActive(false);
                 break;
 
             case 4:
-                //Current_obj.SetActive(false);
-                //Current_obj2.SetActive(false);
                 Current_obj3.SetActive(false);
                 Current_obj4.SetActive(true);
-                //Current_obj5.SetActive(false);
                 break;
 
             case 5:
-                //Current_obj.SetActive(false);
-                //Current_obj2.SetActive(false);
-                //Current_obj3.SetActive(false);
                 Current_obj4.SetActive(false);
                 Current_obj5.SetActive(true);
                 break;
